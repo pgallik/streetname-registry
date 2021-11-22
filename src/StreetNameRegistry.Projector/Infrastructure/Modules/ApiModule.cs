0188666f48b1882bc3264a332932718f61ebb74d
Arne Dumarey <dumarey.arne@gmail.com>
2021-11-22 15:03:14 +0100

    fix: don't run V2 of extract!

--- src/StreetNameRegistry.Projector/Infrastructure/Modules/ApiModule.cs
+++ src/StreetNameRegistry.Projector/Infrastructure/Modules/ApiModule.cs
@@ -87,10 +87,10 @@ namespace StreetNameRegistry.Projector.Infrastructure.Modules
                     _loggerFactory)
                 .RegisterProjections<StreetNameExtractProjections, ExtractContext>(
                     context => new StreetNameExtractProjections(context.Resolve<IOptions<ExtractConfig>>(), DbaseCodePage.Western_European_ANSI.ToEncoding()),
-                    ConnectedProjectionSettings.Default)
-                .RegisterProjections<StreetNameExtractProjectionsV2, ExtractContext>(
-                    context => new StreetNameExtractProjectionsV2(context.Resolve<IOptions<ExtractConfig>>(), DbaseCodePage.Western_European_ANSI.ToEncoding()),
                     ConnectedProjectionSettings.Default);
+                //.RegisterProjections<StreetNameExtractProjectionsV2, ExtractContext>(
+                //    context => new StreetNameExtractProjectionsV2(context.Resolve<IOptions<ExtractConfig>>(), DbaseCodePage.Western_European_ANSI.ToEncoding()),
+                //    ConnectedProjectionSettings.Default);
         }
 
         private void RegisterLastChangedProjections(ContainerBuilder builder)
