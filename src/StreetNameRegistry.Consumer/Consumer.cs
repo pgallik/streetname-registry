054fa092986c363bcce432e87eb64b9c103460ba
Arne Dumarey <dumarey.arne@gmail.com>
2022-01-28 16:02:30 +0100

    fix: update message handling

--- src/StreetNameRegistry.Consumer/Consumer.cs
+++ src/StreetNameRegistry.Consumer/Consumer.cs
@@ -1,18 +1,13 @@
 namespace StreetNameRegistry.Consumer
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
     using Autofac;
     using Be.Vlaanderen.Basisregisters.GrAr.Contracts.MunicipalityRegistry;
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
     using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple;
-    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple.Extensions;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
-    using Confluent.Kafka;
-    using Newtonsoft.Json;
     using NodaTime.Extensions;
     using StreetName.Commands;
 
@@ -83,108 +78,4 @@ namespace StreetNameRegistry.Consumer
             Console.WriteLine("bla bla");
         }
     }
-
-    public static class KafkaConsumer
-    {
-        public static async Task<Result> Consume(
-            KafkaOptions options,
-            string consumerGroupId,
-            string topic,
-            Func<object, Task> messageHandler,
-            CancellationToken cancellationToken = default)
-        {
-            var config = new ConsumerConfig
-            {
-                BootstrapServers = options.BootstrapServers,
-                GroupId = consumerGroupId,
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit = false
-            };
-
-            var serializer = JsonSerializer.CreateDefault(options.JsonSerializerSettings);
-
-            using var consumer = new ConsumerBuilder<Ignore, string>(config)
-                .SetValueDeserializer(Deserializers.Utf8)
-                .Build();
-            try
-            {
-                consumer.Subscribe(topic);
-
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    var consumeResult = consumer.Consume(TimeSpan.FromSeconds(3));
-                    if(consumeResult == null)
-                        continue;
-                    
-                    var kafkaJsonMessage = serializer.Deserialize<KafkaJsonMessage>(consumeResult.Message.Value) ?? throw new ArgumentException("Kafka json message is null.");
-                    var messageData = kafkaJsonMessage.Map() ?? throw new ArgumentException("Kafka message data is null.");
-
-                    await messageHandler(messageData);
-                    consumer.Commit(consumeResult);
-                }
-
-                return Result.Success();
-            }
-            catch (ConsumeException ex)
-            {
-                return Result.Failure(ex.Error.Code.ToString(), ex.Error.Reason);
-            }
-            catch (OperationCanceledException)
-            {
-                return Result.Success();
-            }
-            finally
-            {
-                consumer.Unsubscribe();
-            }
-        }
-    }
-
-    public class KafkaJsonMessage
-    {
-        public string Type { get; set; }
-        public string Data { get; set; }
-
-        public KafkaJsonMessage(string type, string data)
-        {
-            Type = type;
-            Data = data;
-        }
-
-        public object Map()
-        {
-            var assembly = GetAssemblyNameContainingType(Type);
-            var type = assembly.GetType(Type);
-
-            return JsonConvert.DeserializeObject(Data, type!)!;
-        }
-
-        private static Assembly? GetAssemblyNameContainingType(string typeName)
-        {
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                var t = assembly.GetType(typeName, false, true);
-                if (t != null) { return assembly; }
-            }
-
-            return null;
-        }
-
-        public static KafkaJsonMessage Create<T>([DisallowNull] T message, JsonSerializer serializer)
-        {
-            if (message == null)
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
-
-            if (serializer == null)
-            {
-                throw new ArgumentNullException(nameof(serializer));
-            }
-
-            return new KafkaJsonMessage(message.GetType().FullName!, serializer.Serialize(message));
-        }
-    }
-
-
 }
