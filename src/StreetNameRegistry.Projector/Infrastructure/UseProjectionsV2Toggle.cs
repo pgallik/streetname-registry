b2f3a48c62f18aa7c20c8c3c84dbf0738a7db56b
Arne Dumarey <dumarey.arne@gmail.com>
2022-03-04 10:39:41 +0100

    fix: add missing files

---
+++ src/StreetNameRegistry.Projector/Infrastructure/UseProjectionsV2Toggle.cs
@@ -0,0 +1,19 @@
+namespace StreetNameRegistry.Projector.Infrastructure
+{
+    using FeatureToggle;
+    public class FeatureToggleOptions
+    {
+        public const string ConfigurationKey = "FeatureToggles";
+        public bool UseProjectionsV2 { get; set; }
+    }
+
+    public class UseProjectionsV2Toggle : IFeatureToggle
+    {
+        public bool FeatureEnabled { get; }
+
+        public UseProjectionsV2Toggle(bool featureEnabled)
+        {
+            FeatureEnabled = featureEnabled;
+        }
+    }
+}
