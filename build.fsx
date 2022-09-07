05f8590d181e0e17e144196326b5f9608849c9d1
jvandaal <jonas.vandaal@outlook.be>
2022-09-07 14:56:06 +0200

    build: clean-up publish

--- build.fsx
+++ build.fsx
@@ -48,7 +48,6 @@ Target.create "Publish_Solution" (fun _ ->
     "StreetNameRegistry.Projector"
     "StreetNameRegistry.Api.BackOffice"
     "StreetNameRegistry.Api.BackOffice.Abstractions"
-    "StreetNameRegistry.Api.BackOffice.Handlers.Sqs"
     "StreetNameRegistry.Api.BackOffice.Handlers.Sqs.Lambda"
     "StreetNameRegistry.Api.Legacy"
     "StreetNameRegistry.Api.Oslo"
