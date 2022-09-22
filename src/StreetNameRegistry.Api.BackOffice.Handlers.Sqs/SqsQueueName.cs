ccaabd07bbf35cc797ad5d0c8d2dec90efdf0004
Arne Dumarey <dumarey.arne@gmail.com>
2022-09-22 08:57:43 +0200

    fix: correct queue name

--- src/StreetNameRegistry.Api.BackOffice.Handlers.Sqs/SqsQueueName.cs
+++ src/StreetNameRegistry.Api.BackOffice.Handlers.Sqs/SqsQueueName.cs
@@ -2,6 +2,6 @@ namespace StreetNameRegistry.Api.BackOffice.Handlers.Sqs
 {
     internal static class SqsQueueName
     {
-        public const string Value = $"{nameof(StreetNameRegistry)}.{nameof(Api)}.{nameof(BackOffice)}";
+        public const string Value = "sqs-streetname-registry";
     }
 }
