4333f11154b58294cbe9422d498620a57c4789b3
jvandaal <jonas.vandaal@outlook.be>
2022-10-11 15:58:56 +0200

    fix: correct address approval missing validator

---
+++ src/StreetNameRegistry.Api.BackOffice/Validators/StreetNameCorrectApprovalRequestValidator.cs
@@ -0,0 +1,9 @@
+namespace StreetNameRegistry.Api.BackOffice.Validators
+{
+    using Abstractions.Requests;
+    using FluentValidation;
+
+    public sealed class StreetNameCorrectApprovalRequestValidator : AbstractValidator<StreetNameCorrectApprovalRequest>
+    {
+    }
+}
