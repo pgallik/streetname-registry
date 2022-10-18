b780c0ba084f94aa8a2b162d584bcd90239d4b57
pgallik <patrickgallik@gmail.com>
2022-10-18 16:37:36 +0200

    merge

--- src/StreetNameRegistry.Api.BackOffice.Abstractions/Requests/StreetNameRejectRequest.cs
+++ src/StreetNameRegistry.Api.BackOffice.Abstractions/Requests/StreetNameRejectRequest.cs
@@ -1,7 +1,7 @@
 namespace StreetNameRegistry.Api.BackOffice.Abstractions.Requests
 {
     using System.Collections.Generic;
-    using System.Runtime.Serialization;
+    using Be.Vlaanderen.Basisregisters.Sqs.Responses;
     using MediatR;
     using Newtonsoft.Json;
 
